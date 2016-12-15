51ba3479009a99383011bfbe19e26a4072a45fb0
Adam Talbot <adam.talbot@skybettingandgaming.com>
2016-12-15 13:21:49 +0000

    Added watch argument

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,21 @@ import {mutate} from './mutate';
 import {lock} from './lock';
 import * as yargs from 'yargs';
 
-async function main(lockfile : string, token : string, channel : string, user? : string, avatar? : string) {
+async function tick(lockfile : string, token : string, channel : string, user? : string, avatar? : string) {
     let info = await fetchXKCD();
     let newer = await lock(lockfile, info.num);
     if (newer) {
         let message = defaultMessage(token, channel, user, avatar);
         let mutated = mutate(info, message);
         await postMessage(mutated);
+    }
+}
+
+async function main(args : any) {
+    tick(args.l, args.t, args.c, args.n, args.a);
+
+    if(args.w) {
+        setInterval(tick, 1000 * 60 * 30, args.l, args.t, args.c, args.n, args.a)
     }
 }
 
@@ -45,7 +53,13 @@ let args = yargs
         describe: 'bot avatar url',
         type: 'string',
     })
+    .option('w', {
+        alias: 'watch',
+        default: false,
+        describe: 'watch for changes',
+        type: 'boolean',
+    })
     .help()
     .argv;
 
-main(args['l'], args['t'], args['c'], args['n'], args['a']);
+main(args);
